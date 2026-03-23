bb6f85302636285aafb08f194acdf32863b1e70e
Дмитрий Ушанёв <ushanevdimhon@gmail.com>
2026-03-23 12:59:18 +0500

    обновил конфиг и .env для использования прокси

--- config.py
+++ config.py
@@ -7,7 +7,19 @@ load_dotenv()
 # Получаем переменные окружения
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 API_KEY = os.getenv('API_KEY')
+PROXY_LOGIN = os.getenv("PROXY_LOGIN")
+PROXY_PASSWORD = os.getenv("PROXY_PASSWORD")
+PROXY_IP = os.getenv("PROXY_IP")
+PROXY_PORT = os.getenv("PROXY_PORT")
 
 # Проверка, что ключи загружены
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не найден в .env файле")
+if not PROXY_LOGIN:
+    raise ValueError("PROXY_LOGIN не найден в .env файле")
+if not PROXY_PASSWORD:
+    raise ValueError("PROXY_PASSWORD не найден в .env файле")
+if not PROXY_IP:
+    raise ValueError("PROXY_IP не найден в .env файле")
+if not PROXY_PORT:
+    raise ValueError("PROXY_PORT не найден в .env файле")
