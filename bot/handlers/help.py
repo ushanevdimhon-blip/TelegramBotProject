a35710ac444dd3c75dc5197ce579e92c76b61e0d
kirill <k_maystruk@bk.ru>
2026-03-25 21:34:18 +0500

    Перенес обработку команды help в отдельный файл

---
+++ bot/handlers/help.py
@@ -0,0 +1,13 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
+router = Router()
+
+@router.message(Command("help"))
+async def c_help(message: Message):
+    #TODO: Внести сюда список доступных команд с объяснением их работы
+    """Обработчик команды /help"""
+    await message.answer(
+        "/start - начать работу бота"
+    )
