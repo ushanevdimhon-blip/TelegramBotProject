f2c59063dece1338ac9e05245d3cf771a66a5132
kirill <k_maystruk@bk.ru>
2026-03-25 21:53:21 +0500

    рефакторинг: добавил комментарии

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,3 +1,3 @@
 from . import start, help, info
 
-__all__ = ['start', 'help', 'info']
+__all__ = ['start', 'help', 'info'] #Список того что можно импортировать отсюда
--- bot/main.py
+++ bot/main.py
@@ -24,4 +24,4 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print('❌ Сервер не запущен')
+        print('~~Сервер не запущен~~')
