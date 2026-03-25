e8a67a7d41ad2a2ebd9c366de7ac2fde0433da11
kirill <k_maystruk@bk.ru>
2026-03-25 21:49:35 +0500

    рефакторинг: изменил имена функции, добавил еще одну команду в описание

--- bot/handlers/help.py
+++ bot/handlers/help.py
@@ -5,9 +5,10 @@ from aiogram.filters import Command
 router = Router()
 
 @router.message(Command("help"))
-async def c_help(message: Message):
+async def cmd_help(message: Message):
     #TODO: Внести сюда список доступных команд с объяснением их работы
     """Обработчик команды /help"""
     await message.answer(
         "/start - начать работу бота"
+        "/info - посмотреть информацию о доступных командах"
     )
