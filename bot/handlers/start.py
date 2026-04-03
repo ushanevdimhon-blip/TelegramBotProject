eeb2bae35cb19bedce52fbab89c96c56df539792
kirill <k_maystruk@bk.ru>
2026-04-03 20:30:18 +0500

    Обновил функционал обработчика /start, теперь при старте происходить регистрация в sheets

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -2,17 +2,36 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 from aiogram.utils.markdown import hbold
+from services.sheets import get_sheets_service
 from config import START_MESSAGE_EFFECT
+
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    """Обработчик команды /start"""
-    user_last_name = message.from_user.last_name or ''
+    """Обработчик команды /start (Регистрация пользователя)"""
+    user_id = message.from_user.id
+    username = message.from_user.username or "empty"
+    full_name = message.from_user.full_name or "username"
 
-    await message.answer(
-        f'Здарова, {hbold(user_last_name).replace("<b>", "").replace("</b>", "")}!',
-        message_effect_id=START_MESSAGE_EFFECT
-    )
+    sheets = get_sheets_service()
+
+    if not sheets:
+        await message.answer("Google sheets временно недоступен, попробуйте позже")
+        return
+
+    if sheets.add_user(telegram_id = user_id, username = username):
+        await message.answer(
+            f"Привет, {full_name}!\n\n"
+            f"Вы зарегистрированы в системе.\n"
+            f"Используйте /submit чтобы сдать работу\n"
+            f"Используйте /next чтобы взять работу на проверку",
+            message_effect=START_MESSAGE_EFFECT
+        )
+    else:
+        await message.answer(
+            f"Привет, {full_name}!\n"
+            f"Нам не удалось вас зарегистрировать, пожалуйста, попробуйте позже"
+        )
