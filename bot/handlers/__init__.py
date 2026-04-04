574ebfce0889e82675d9a3602b732a12bbdfa297
kirill <k_maystruk@bk.ru>
2026-04-05 00:38:20 +0500

    Обновил список доступных для использования команд

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,3 +1,3 @@
 from . import start, help, info
 
-__all__ = ['start', 'help', 'info'] #Список того что можно импортировать отсюда
+__all__ = ['start', 'help', 'info','next'] #Список того что можно импортировать отсюда
